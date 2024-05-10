cce371a6eb886dac949b33833b25090728629c6e
Bharat Bhushan <134364003+BBhushan098@users.noreply.github.com>
2024-05-10 13:26:44 +0530

    error page added

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import { Button } from "../assets/js/Button";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 function Error() {
-  return <div>Not found</div>;
+  return (
+    <Wrapper>
+      <div className="text-center div">
+        <h2>404</h2>
+        <h2>UH OH! You're lost.</h2>
+        <p>
+          The page you are looking for does not exist. How you get here is a
+          mystery. But you can click the button below to go back to homepage
+        </p>
+        <NavLink to="/">
+          <Button>Go Back to Home</Button>
+        </NavLink>
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section`
+  .div {
+    height: 30rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
 
 export default Error;
